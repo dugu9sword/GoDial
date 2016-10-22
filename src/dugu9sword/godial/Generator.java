9faff131dc4ac30b9488a098cdcb33d024c35e99
dugu9sword <dugu9sword@163.com>
2016-10-22 23:18:10 +0800

    draft

---
+++ src/dugu9sword/godial/Generator.java
@@ -0,0 +1,7 @@
+package dugu9sword.godial;
+
+/**
+ * Created by Yi Zhou on 2016/10/22.
+ */
+public interface Generator {
+}
---
+++ src/dugu9sword/godial/GoDial.java
@@ -0,0 +1,8 @@
+package dugu9sword.godial;
+
+/**
+ * Created by Yi Zhou on 2016/10/22.
+ */
+public class GoDial {
+    private Kernel kernel;
+}
